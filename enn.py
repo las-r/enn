65eb37fad86cf866fe26d94fc1e114607a6c1bd8
Nayif Ehan <128936184+las-r@users.noreply.github.com>
2026-02-14 21:48:53 -0500

    Update enn.py

--- enn.py
+++ enn.py
@@ -1,8 +1,9 @@
+import keyboard
 import re
 import sys
 import time
 
-# enn v1.0.1
+# enn v1.1
 # made by las-r on github, mit license
 
 # helper functions
@@ -14,40 +15,55 @@ def getVar(v):
 if len(sys.argv) < 2:
     print("Usage: python script.py <filename>")
     sys.exit(1)
-if len(sys.argv) > 2: wait = float(sys.argv[2])
-else: wait = 0
+if len(sys.argv) > 2: 
+    wait = float(sys.argv[2])
+else: 
+    wait = 0
 
 # parse file
-if sys.argv[1].endswith(".enn"): pname = sys.argv[1]
+if sys.argv[1].endswith(".enn"): 
+    pname = sys.argv[1]
 else: pname = sys.argv[1] + ".enn"
-with open(pname) as pfile: prog = pfile.read()
+with open(pname) as pfile: 
+    prog = pfile.read()
 prog = re.sub(r'\/.*?\/', '', prog)
 prog = prog.replace("\n", "").replace(" ", "")
-print(prog)
 prog = [p for p in prog.split(";") if p]
 
 # program loop
 var = {0: 0, 1: 1}
-b = False
-while True:
-    for i, p in enumerate(prog):
-        if p[-1] == "=":
-            if p[:-1] not in var: var[p[:-1]] = 0b0;
-                
-        elif "=" in p:
-            q = p.split("=")
-            qa = q[1].split(",")[0]
-            qb = q[1].split(",")[1]
-            var[q[0]] = int(not (getVar(qa) and getVar(qb)))
-    
-        elif p[0] == ">":
-            if p[1:]: print(getVar(p[1:]), end="", flush=True)
-            else: print()
-            
-        else:
-            if p in var: 
-                if getVar(p): var[p] = 0
-                else: var[p] = 1
+try:
+    while True:
+        for i, p in enumerate(prog):
+            if p[-1] == "=":
+                if p[:-1] not in var: var[p[:-1]] = 0b0; #type: ignore
+                    
+            elif "=" in p:
+                q = p.split("=")
+                qa = q[1].split(",")[0]
+                qb = q[1].split(",")[1]
+                var[q[0]] = int(not (getVar(qa) and getVar(qb))) #type: ignore
         
-    time.sleep(wait)
-    if b: break
+            elif p[0] == ">":
+                if p[1:]: 
+                    print(getVar(p[1:]), end="", flush=True)
+                else: 
+                    print()
+                    
+            elif p[0] == "<":
+                if p[1:]:
+                    var[p[1:]] = int(keyboard.is_pressed("space")) #type: ignore
+                    anykey = False
+                else:
+                    while not anykey:
+                        time.sleep(0.01)
+                    anykey = False
+                
+            else:
+                if p in var:
+                    var[p] = 1 - getVar(p)
+            
+        time.sleep(wait)
+    
+except KeyboardInterrupt:
+    keyboard.unhook_all()
