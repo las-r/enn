53c4e5ae1bed33d370968fb7c995b013ece74540
Nayif Ehan <128936184+las-r@users.noreply.github.com>
2026-02-17 22:33:14 -0500

    Update enn.py

--- enn.py
+++ enn.py
@@ -1,29 +1,63 @@
+import argparse
 import keyboard
 import re
-import sys
 import time
 
-# enn v1.1.2
+# enn v1.2
 # made by las-r on github, mit license
 
 # helper functions
 def getVar(v):
-    if v in var: return var[v]
-    else: return 0
+    if v in var: 
+        return var[v]
+    else: 
+        return 0
+def checkName(v):
+    if v.includes("=") or v.includes(" "):
+        return
+    else:
+        return v
 
-# open file
-if len(sys.argv) < 2:
-    print("Usage: python script.py <filename>")
-    sys.exit(1)
-if len(sys.argv) > 2: 
-    wait = float(sys.argv[2])
-else: 
-    wait = 0
+# argument parsing
+parser = argparse.ArgumentParser(
+    prog="enn",
+    description="Enn v1.2 - NAND-based esolang interpreter"
+)
+parser.add_argument("filename", help="Program file to run")
+parser.add_argument(
+    "-w", "--wait",
+    type=float,
+    default=0,
+    help="Delay between execution cycles (seconds)"
+)
+parser.add_argument(
+    "-o", "--once",
+    action="store_true",
+    help="Run program once instead of looping forever"
+)
+parser.add_argument(
+    "-d", "--debug",
+    action="store_true",
+    help="Print variable state after each cycle"
+)
+parser.add_argument(
+    "-ne", "--no-ext",
+    action="store_true",
+    help="Do not automatically append .enn"
+)
+parser.add_argument(
+    "-v", "--version",
+    action="version",
+    version="Enn v1.2"
+)
+args = parser.parse_args()
+wait = args.wait
 
 # parse file
-if sys.argv[1].endswith(".enn"): 
-    pname = sys.argv[1]
-else: pname = sys.argv[1] + ".enn"
+if args.no_ext:
+    pname = args.filename
+else:
+    pname = args.filename if args.filename.endswith(".enn") else args.filename + ".enn"
 with open(pname) as pfile: 
     prog = pfile.read()
 prog = re.sub(r'\/.*?\/', '', prog)
@@ -33,23 +67,35 @@ prog = [p for p in prog.split(";") if p]
 # program loop
 var = {"0": 0, "1": 1}
 try:
-    while True:
+    running = True
+    while running:
         for i, p in enumerate(prog):
+            # variable set
             if p[-1] == "=":
-                if p[:-1] not in var: var[p[:-1]] = 0b0; #type: ignore
-                    
+                if p[:-1] not in var: 
+                    var[checkName(p[:-1])] = 0; #type: ignore
+                
             elif "=" in p:
                 q = p.split("=")
                 qa = q[1].split(",")[0]
                 qb = q[1].split(",")[1]
-                var[q[0]] = int(not (getVar(qa) and getVar(qb))) #type: ignore
+                var[checkName(q[0])] = int(not (getVar(qa) and getVar(qb))) #type: ignore
         
+            # output
+            elif p.startswith(">>"):
+                c = "".join(p[2:].split(","))
+                if len(c) == 8:
+                    b = int(c, 2)
+                    d = b.to_bytes(len(c) // 8, "big").decode("utf-8")
+                    print(d, end="", flush=True)
+                    
             elif p[0] == ">":
                 if p[1:]: 
                     print(getVar(p[1:]), end="", flush=True)
                 else: 
                     print()
-                    
+            
+            # input        
             elif p[0] == "<":
                 if p[1:]:
                     var[p[1:]] = int(keyboard.is_pressed("space")) #type: ignore
@@ -62,7 +108,11 @@ try:
             else:
                 if p in var:
                     var[p] = 1 - getVar(p)
-            
+        
+        if args.debug:
+            print("\n[DEBUG]", var)
+        if args.once:
+            running = False
         time.sleep(wait)
     
 except KeyboardInterrupt:
